391e779ebd97dd402f68f0424ee4be87af58bbed
maycsy96 <may@apptivitylab.com>
2018-01-17 17:40:44 +0800

    Added PetrolStationDetailFragment, rename RecentActivity to SearchActivity

---
+++ app/src/main/java/learn/apptivitylab/com/petrolnav/ui/PetrolStationDetailFragment.kt
@@ -0,0 +1,7 @@
+package learn.apptivitylab.com.petrolnav.ui
+
+/**
+ * Created by apptivitylab on 17/01/2018.
+ */
+class PetrolStationDetailFragment {
+}
--- app/src/main/java/learn/apptivitylab/com/petrolnav/ui/SearchFragment.kt
+++ app/src/main/java/learn/apptivitylab/com/petrolnav/ui/SearchFragment.kt
@@ -2,6 +2,7 @@ package learn.apptivitylab.com.petrolnav.ui
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v4.widget.SwipeRefreshLayout
 import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
@@ -14,7 +15,7 @@ import learn.apptivitylab.com.petrolnav.model.PetrolStation
  * Created by apptivitylab on 09/01/2018.
  */
 
-class SearchFragment : Fragment(), SearchAdapter.StationViewHolder.onSelectStationListener {
+class SearchFragment : Fragment(), SearchAdapter.StationViewHolder.onSelectStationListener, SwipeRefreshLayout.OnRefreshListener {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_search, container, false)
@@ -31,6 +32,10 @@ class SearchFragment : Fragment(), SearchAdapter.StationViewHolder.onSelectStati
         petrolStationListRecyclerView.adapter = petrolStationListAdapter
         //petrolStationListAdapter.updateDataSet()
 
+    }
+
+    override fun onRefresh() {
+        TODO("not implemented")
     }
     override fun onStationSelected(station: PetrolStation) {
         TODO("not implemented")
