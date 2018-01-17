b0fb4d2850c79d22c9c00ba657c4ddc6549ca545
maycsy96 <may@apptivitylab.com>
2018-01-17 14:36:39 +0800

    Added constructor for PetrolStation

--- app/src/main/java/learn/apptivitylab/com/petrolnav/model/PetrolStation.kt
+++ app/src/main/java/learn/apptivitylab/com/petrolnav/model/PetrolStation.kt
@@ -1,14 +1,26 @@
 package learn.apptivitylab.com.petrolnav.model
 
+import com.google.android.gms.maps.model.LatLng
+import org.json.JSONObject
+
 /**
  * Created by apptivitylab on 12/01/2018.
  */
 data class PetrolStation(
-        var id:Int,
-        var name:String,
-        var address:String,
-        var operatingHours: List<OperatingHour>?,
-        var petrol:List<Petrol>?,
-        var petrolStationBrand: PetrolStationBrand?) {
+        var petrolStationId:String,
+        var petrolStationName:String,
+        var petrolStationAddress:String,
+        var petrolStationLatLng:LatLng)
+//        var petrolStationOperatingHour: List<OperatingHour>?,
+//        var petrol:List<Petrol>?,
+//        var petrolStationBrand: PetrolStationBrand)
+{
+
+    constructor(jsonObject : JSONObject){
+        this.petrolStationId = jsonObject.optString("ID:")
+        this.petrolStationName = jsonObject.optString("Name:")
+        this.petrolStationAddress = jsonObject.optString("Address")
+        this.petrolStationLatLng = LatLng(jsonObject.optDouble("Latitude"),jsonObject.optDouble("Longitude"))
+    }
 
 }
