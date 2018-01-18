dc3523907205c490d636b1920fb2332b3cb94ca8
maycsy96 <may@apptivitylab.com>
2018-01-18 17:26:26 +0800

    Remove unused TAG

--- app/src/main/java/learn/apptivitylab/com/petrolnav/controller/PetrolStationLoader.kt
+++ app/src/main/java/learn/apptivitylab/com/petrolnav/controller/PetrolStationLoader.kt
@@ -1,13 +1,37 @@
 package learn.apptivitylab.com.petrolnav.controller
 
+import android.content.Context
+import com.google.android.gms.maps.model.LatLng
+import learn.apptivitylab.com.petrolnav.R
+import learn.apptivitylab.com.petrolnav.model.PetrolStation
+import java.io.BufferedReader
+import java.io.InputStream
+import java.io.InputStreamReader
+
 /**
  * Created by apptivitylab on 16/01/2018.
  */
 class PetrolStationLoader {
 
+    fun loadStations (context: Context) : ArrayList<PetrolStation> {
+        val listOfStations: ArrayList<PetrolStation> = ArrayList()
 
-    companion object {
-        val TAG = " PetrolStationLoader"
+        val inputStream : InputStream = context.resources.openRawResource(R.raw.stations)
+        val reader : BufferedReader = BufferedReader(InputStreamReader(inputStream))
+
+        reader.forEachLine {
+            val tokens : List<String> = it.split(";")
+
+            val stationID = tokens[0]
+            val stationName = tokens[1]
+            val stationBrand = tokens[2]
+            val stationLatLng = LatLng(tokens[3].toDouble(), tokens[4].toDouble())
+
+            val station : PetrolStation = PetrolStation(stationID, stationName, stationBrand, stationLatLng)
+            listOfStations.add(station)
+        }
+
+        return listOfStations
     }
 
 }
