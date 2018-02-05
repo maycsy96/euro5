7e720e841be3834d0cb0c7cbe6f052bda6081282
maycsy96 <may@apptivitylab.com>
2018-02-05 18:09:07 +0800

    Rename the variable to be shorter and check whether user email existed in the file

--- app/src/main/java/learn/apptivitylab/com/petrolnav/ui/RegisterActivity.kt
+++ app/src/main/java/learn/apptivitylab/com/petrolnav/ui/RegisterActivity.kt
@@ -1,5 +1,6 @@
 package learn.apptivitylab.com.petrolnav.ui
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
@@ -7,6 +8,10 @@ import android.util.Log
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_register.*
 import learn.apptivitylab.com.petrolnav.R
+import learn.apptivitylab.com.petrolnav.model.User
+import org.json.JSONArray
+import java.io.FileNotFoundException
+import java.io.IOException
 
 /**
  * Created by apptivitylab on 08/01/2018.
@@ -14,11 +19,28 @@ import learn.apptivitylab.com.petrolnav.R
 
 class RegisterActivity : AppCompatActivity() {
 
+    companion object {
+        private val TAG = "RegisterActivity"
+        private val ARG_USER_LIST = "user_list"
+
+        fun newLaunchIntent(context: Context, userList: ArrayList<User>): Intent {
+            val intent = Intent(context, RegisterActivity::class.java)
+            intent.putExtra(ARG_USER_LIST, userList)
+            return intent
+        }
+    }
+
+    private var userList = ArrayList<User>()
+    private var user = User()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
+        this.userList = intent.getParcelableArrayListExtra(ARG_USER_LIST)
+
         this.registerButton.setOnClickListener { register() }
+
         this.loginTextView.setOnClickListener {
             val intent = Intent(applicationContext, LoginActivity::class.java)
             startActivity(intent)
@@ -56,35 +78,46 @@ class RegisterActivity : AppCompatActivity() {
     fun validate(): Boolean {
         var valid = true
 
-        val name = this.registerNameEditText.text.toString()
-        val email = this.registerEmailEditText.text.toString()
-        val password = this.registerPasswordEditText.text.toString()
+        val name = this.nameEditText.text.toString()
+        val email = this.emailEditText.text.toString()
+        val password = this.passwordEditText.text.toString()
+        var confirmPassword = this.confirmPasswordEditText.text.toString()
 
         if (name.isEmpty() || name.length < 3) {
-            this.registerNameEditText.error = "at least 3 characters"
+            this.nameEditText.error = "at least 3 characters"
             valid = false
         } else {
-            this.registerNameEditText.error = null
+            this.nameEditText.error = null
         }
 
         if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-            this.registerEmailEditText.error = "enter a valid email address"
+            this.emailEditText.error = "enter a valid email address"
             valid = false
         } else {
-            this.registerEmailEditText.error = null
+            this.emailEditText.error = null
         }
 
-        if (password.isEmpty() || password.length < 4 || password.length > 10) {
-            this.registerPasswordEditText.error = "between 4 and 10 alphanumeric characters"
+        if (password.isEmpty() || password.length < 4) {
+            this.passwordEditText.error = "at least 4 alphanumeric characters"
             valid = false
         } else {
-            this.registerPasswordEditText.error = null
+            this.confirmPasswordEditText.error = null
+        }
+
+        if (confirmPassword.isEmpty()) {
+            this.confirmPasswordEditText.error = "enter password to confirm"
+        } else if (confirmPassword != password) {
+            this.confirmPasswordEditText.error = "It is not same as the password you input"
+        } else {
+            this.confirmPasswordEditText.error = null
+        }
+
+        var userEmail = this.userList.firstOrNull{ it.userEmail == email}
+        if(userEmail != null){
+            this.emailEditText.error = "the email had been used. Please use other email"
+            valid = false
         }
 
         return valid
-    }
-
-    companion object {
-        private val TAG = "RegisterActivity"
     }
 }
