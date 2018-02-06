c027a86a61d1924c96894dfceda848b8930f1176
maycsy96 <may@apptivitylab.com>
2018-02-06 12:04:00 +0800

    Replace loadJSONUser with loadJSONUserList to load users' list instead of a single user

--- app/src/main/java/learn/apptivitylab/com/petrolnav/controller/UserController.kt
+++ app/src/main/java/learn/apptivitylab/com/petrolnav/controller/UserController.kt
@@ -16,20 +16,26 @@ class UserController {
     companion object {
         private val TAG = "UserController"
 
-        fun loadJSONUser(context: Context): User? {
-            val fileName = "user.txt"
-
+        fun loadJSONUserList(context:Context): ArrayList<User>{
+            val fileName = "userlist.json"
             try {
                 val inputStream = BufferedInputStream(context.openFileInput(fileName))
                 val data = ByteArray(inputStream.available())
                 inputStream.read(data)
                 inputStream.close()
 
-                val jsonObjectString = String(data)
-                val jsonObject = JSONObject(jsonObjectString)
-
-                val user = User(jsonObject)
-                return user
+                val jsonArrayString = String(data)
+                val jsonArray = JSONArray(jsonArrayString)
+                val userList: ArrayList<User> = ArrayList<User>()
+                for(i in 0..jsonArray.length()-1){
+                    try{
+                        var user = User(jsonArray.getJSONObject(i))
+                        userList.add(user)
+                    }catch(e: JSONException){
+                        e.printStackTrace()
+                    }
+                }
+                return userList
 
             } catch (e: FileNotFoundException) {
                 e.printStackTrace()
@@ -38,7 +44,24 @@ class UserController {
             } catch (e: JSONException) {
                 e.printStackTrace()
             }
-            return null
+            return ArrayList<User>()
+        }
+
+        fun writeToJSONUserList(context:Context, userList: ArrayList<User>){
+            var userListJsonArray = JSONArray()
+            val fileName = "userlist.json"
+            for (user in userList) {
+                userListJsonArray.put(user.toJsonObject())
+            }
+            try {
+                val outputStream = context.openFileOutput(fileName, Context.MODE_PRIVATE)
+                outputStream.write(userListJsonArray.toString().toByteArray())
+                outputStream.close()
+            } catch (e: FileNotFoundException) {
+                e.printStackTrace()
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
         }
     }
 }
