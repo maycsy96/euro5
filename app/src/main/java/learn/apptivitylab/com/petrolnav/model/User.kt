0f874d61377490f8a1ad899f426511b044b2d64c
maycsy96 <may@apptivitylab.com>
2018-02-06 12:04:32 +0800

    Add check for null to ensure Petrol object is not constructed if it is null

--- app/src/main/java/learn/apptivitylab/com/petrolnav/model/User.kt
+++ app/src/main/java/learn/apptivitylab/com/petrolnav/model/User.kt
@@ -40,7 +40,9 @@ data class User(var userId: String? = null,
         this.userName = jsonObject.optString("user_name")
         this.userEmail = jsonObject.optString("user_email")
         this.userPassword = jsonObject.optString("user_password")
-        this.userPreferredPetrol = Petrol(jsonObject?.optJSONObject("petrol"))
+        if (!jsonObject?.optJSONObject("petrol").optString("petrol_id").isNullOrEmpty()){
+            this.userPreferredPetrol = Petrol(jsonObject?.optJSONObject("petrol"))
+        }
 
         var petrolStationBrand: PetrolStationBrand
         var petrolStationBrandListJsonArray = jsonObject.optJSONArray("petrol_station_brands")
