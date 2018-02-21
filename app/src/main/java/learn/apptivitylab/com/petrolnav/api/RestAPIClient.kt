6f774b55fb369a3b3ab1bbad15f8c45a0e276a4b
maycsy96 <may@apptivitylab.com>
2018-02-21 16:49:33 +0800

    Add listener interface and pagination API function

--- app/src/main/java/learn/apptivitylab/com/petrolnav/api/RestAPIClient.kt
+++ app/src/main/java/learn/apptivitylab/com/petrolnav/api/RestAPIClient.kt
@@ -5,8 +5,8 @@ import com.android.volley.Request
 import com.android.volley.RequestQueue
 import com.android.volley.Response
 import com.android.volley.VolleyError
-import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
+import org.json.JSONArray
 import org.json.JSONObject
 
 
@@ -37,20 +37,67 @@ class RestAPIClient(val context: Context) {
         fun onComplete(jsonObject: JSONObject?, error: VolleyError?)
     }
 
+    interface receiveDataSetListener {
+        fun onDataSetReceived(jsonObject: JSONObject?, error: VolleyError?)
     }
 
-    fun loadResource(path: String, completionListener: getResourceCompleteListener) {
+    interface receiveCompleteDataListener {
+        fun onCompleteDataReceived(dataReceived: Boolean, error: VolleyError?)
+    }
+
+    fun loadResource(path: String, limit: Int?, completionListener: getResourceCompleteListener) {
         val url = String.format("%s/%s", BASE_URL, path)
-        val request = newRequest(Request.Method.GET, url, null,
+        if (limit == null) {
+            val request = BackendlessJsonObjectRequest(Request.Method.GET, url, null,
+                    Response.Listener<JSONObject> { response ->
+                        completionListener.onComplete(response, null)
+                    },
+                    Response.ErrorListener { error ->
+                        completionListener.onComplete(null, error)
+                    })
+            this.requestQueue.add(request)
+        } else {
+            var resultJSONArray = JSONArray()
+            this.getNextData(url, limit, 0, resultJSONArray,
+                    object : receiveDataSetListener {
+                        override fun onDataSetReceived(jsonObject: JSONObject?, error: VolleyError?) {
+                            if (jsonObject != null) {
+                                completionListener.onComplete(jsonObject, null)
+                            } else {
+                                completionListener.onComplete(null, error)
+                            }
+                        }
+
+                    })
+        }
+    }
+
+    fun getNextData(path: String, limit: Int = 100, offset: Int = 0, resultJSONArray: JSONArray, receiveDataSetListener: receiveDataSetListener) {
+        var limitParameter = "&limit=" + limit
+        var offsetParameter = "&offset=" + offset
+        val url = String.format("%s%s%s", path, limitParameter, offsetParameter)
+
+        val request = BackendlessJsonObjectRequest(Request.Method.GET, url, null,
                 Response.Listener<JSONObject> { response ->
-                    completionListener.onComplete(response, null)
+                    response?.let {
+                        val jsonArray = it.optJSONArray("resource")
+                        (0 until jsonArray.length()).forEach { item ->
+                            resultJSONArray.put(jsonArray.getJSONObject(item))
+                        }
+                        if (jsonArray.length() < limit || jsonArray.length() == 0) {
+                            var resultJSONObject = JSONObject()
+                            resultJSONObject.put("resource", resultJSONArray)
+                            receiveDataSetListener.onDataSetReceived(resultJSONObject, null)
+                        } else {
+                            this@RestAPIClient.getNextData(url, limit, offset + limit, resultJSONArray, receiveDataSetListener)
+                        }
+                    }
                 },
                 Response.ErrorListener { error ->
-                    completionListener.onComplete(null, error)
+                    receiveDataSetListener.onDataSetReceived(null, error)
                 })
         this.requestQueue.add(request)
     }
-
 }
 
 
