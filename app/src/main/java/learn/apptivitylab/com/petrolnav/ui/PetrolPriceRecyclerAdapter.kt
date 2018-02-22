53bff4a6d6e35e6bd2541b30dbd562c1503d321a
maycsy96 <may@apptivitylab.com>
2018-02-22 14:21:21 +0800

    Remove header and add price change for each price history cell

--- app/src/main/java/learn/apptivitylab/com/petrolnav/ui/PetrolPriceRecyclerAdapter.kt
+++ app/src/main/java/learn/apptivitylab/com/petrolnav/ui/PetrolPriceRecyclerAdapter.kt
@@ -5,10 +5,8 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import kotlinx.android.synthetic.main.cell_header_petrol.view.*
 import kotlinx.android.synthetic.main.cell_price_history.view.*
 import learn.apptivitylab.com.petrolnav.R
-import learn.apptivitylab.com.petrolnav.model.Petrol
 import learn.apptivitylab.com.petrolnav.model.PriceHistory
 import java.text.SimpleDateFormat
 
@@ -18,106 +16,55 @@ import java.text.SimpleDateFormat
  */
 class PetrolPriceRecyclerAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-    companion object {
-        private val HEADER = 0
-        private val ITEM = 1
-    }
-
-    private var petrol: Petrol? = null
+    private var priceHistoryList = ArrayList<PriceHistory>()
 
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): RecyclerView.ViewHolder {
-        return when (viewType) {
-            HEADER -> {
-                PetrolViewHolder(LayoutInflater.from(parent!!.context).inflate(R.layout.cell_header_petrol, parent, false))
-            }
-            else -> {
-                PriceHistoryViewHolder(LayoutInflater.from(parent!!.context).inflate(R.layout.cell_price_history, parent, false))
-            }
-        }
+        return PriceHistoryViewHolder(LayoutInflater.from(parent!!.context).inflate(R.layout.cell_price_history, parent, false))
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        val item = getItem(position)
-
-        when (holder) {
-            is PetrolViewHolder -> {
-                holder.itemView.tag = holder
-                holder.setPetrol(item as Petrol)
-            }
-
-            is PriceHistoryViewHolder -> {
-                holder.itemView.tag = holder
-                holder.setPriceHistory(item as PriceHistory)
-            }
-        }
-    }
-
-    fun getItem(position: Int): Any {
-        val petrolDetailList = ArrayList<Any>()
-        val petrol = this.petrol
-        petrolDetailList.add(petrol as Petrol)
-        petrolDetailList.addAll(petrol.petrolPriceHistoryList)
-
-        return petrolDetailList[position]
+        val priceHistoryHolder = holder as PriceHistoryViewHolder
+        val priceHistory = this.priceHistoryList[position]
+        priceHistoryHolder.setPriceHistory(priceHistory, this.priceHistoryList)
     }
 
     override fun getItemCount(): Int {
-        var historyCount = 0
-        this.petrol?.let {
-            historyCount = it.petrolPriceHistoryList.count()
-        }
-        return 1 + historyCount
+        return this.priceHistoryList.size
     }
 
-    override fun getItemViewType(position: Int): Int {
-        if (getItem(position) is Petrol) {
-            return HEADER
-        } else {
-            return ITEM
-        }
-    }
-
-    fun updateDataSet(petrol: Petrol) {
-        this.petrol = petrol
+    fun updateDataSet(priceHistoryList: ArrayList<PriceHistory>) {
+        this.priceHistoryList.clear()
+        this.priceHistoryList.addAll(priceHistoryList)
         this.notifyDataSetChanged()
-    }
-
-    class PetrolViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        private var petrol: Petrol? = null
-
-        fun setPetrol(petrol: Petrol) {
-            this.petrol = petrol
-            itemView.petrolIdTextView.text = this.petrol?.petrolId
-            itemView.petrolNameTextView.text = this.petrol?.petrolName
-            itemView.petrolPriceTextView.text = itemView.context?.getString(R.string.petrol_price_value, this.petrol?.petrolPrice)
-
-            petrol.petrolPriceChange?.let {
-                if (it != null) {
-                    itemView.petrolPriceChangeTextView.text = String.format("%.2f", it)
-                } else {
-                    itemView.petrolPriceChangeTextView.text = itemView.context.getString(R.string.message_unavailable_price_change)
-                }
-
-
-                if (it < 0.0f) {
-                    itemView.petrolPriceChangeTextView.setTextColor(Color.RED)
-                } else if (it > 0.0f) {
-                    itemView.petrolPriceChangeTextView.setTextColor(Color.GREEN)
-                } else {
-                    itemView.petrolPriceChangeTextView.setTextColor(Color.BLACK)
-                }
-            }
-        }
     }
 
     class PriceHistoryViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         private var priceHistory: PriceHistory? = null
-        val dateFormatter = SimpleDateFormat("dd/MM/yyyy")
+        private val dateFormatter = SimpleDateFormat("dd/MM/yyyy")
 
-        fun setPriceHistory(priceHistory: PriceHistory) {
+        fun setPriceHistory(priceHistory: PriceHistory, priceHistoryList: ArrayList<PriceHistory>) {
             this.priceHistory = priceHistory
-            itemView.dateTextView.text = dateFormatter.format(this.priceHistory?.dateCreated)
-            itemView.priceTextView.text = itemView.context?.getString(R.string.petrol_price_value, this.priceHistory?.price)
+            this.itemView.dateTextView.text = dateFormatter.format(this.priceHistory?.dateCreated)
+            this.itemView.priceTextView.text = itemView.context?.getString(R.string.petrol_price_value, this.priceHistory?.price)
+            this.itemView.priceChangeTextView.text
+
+            val position = priceHistoryList.indexOf(priceHistory)
+            val previousPrice = priceHistoryList[position].price
+            previousPrice?.let {
+                val priceChange = priceHistoryList[position + 1].price?.minus(it)
+                priceChange?.let {
+                    if (it != null) {
+                        this.itemView.priceChangeTextView.text = String.format("%.2f", it)
+                    } else {
+                        this.itemView.priceChangeTextView.text = itemView.context.getString(R.string.message_unavailable_price_change)
+                    }
+                    this.itemView.priceChangeTextView.setTextColor(when {
+                        it < 0.0f -> Color.RED
+                        it > 0.0f -> Color.GREEN
+                        else -> Color.BLACK
+                    })
+                }
+            }
         }
     }
 }
