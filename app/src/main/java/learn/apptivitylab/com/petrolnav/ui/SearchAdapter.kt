7ca34a37401a8f4e5eb43c4a1df528f5259373b4
maycsy96 <may@apptivitylab.com>
2018-02-28 18:07:54 +0800

    Remove header

--- app/src/main/java/learn/apptivitylab/com/petrolnav/ui/SearchAdapter.kt
+++ app/src/main/java/learn/apptivitylab/com/petrolnav/ui/SearchAdapter.kt
@@ -5,7 +5,6 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import kotlinx.android.synthetic.main.cell_header_petrol_station.view.*
 import kotlinx.android.synthetic.main.cell_petrol_station.view.*
 import learn.apptivitylab.com.petrolnav.R
 import learn.apptivitylab.com.petrolnav.model.PetrolStation
@@ -14,11 +13,6 @@ import learn.apptivitylab.com.petrolnav.model.PetrolStation
  * Created by apptivitylab on 17/01/2018.
  */
 class SearchAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-
-    companion object {
-        private val STATION_HEADER = 0
-        private val STATION_ITEM = 1
-    }
 
     private var petrolStationsList: ArrayList<Any> = ArrayList()
     private lateinit var petrolStationListener: StationViewHolder.SelectStationListener
@@ -29,47 +23,22 @@ class SearchAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): RecyclerView.ViewHolder {
 
-        var viewHolder: RecyclerView.ViewHolder = when (viewType) {
-            STATION_ITEM -> {
-                StationViewHolder(LayoutInflater.from(parent!!.context).inflate(R.layout.cell_petrol_station,
-                        parent, false), petrolStationListener)
-            }
-            else -> {
-                HeaderViewHolder(LayoutInflater.from(parent!!.context).inflate(R.layout.cell_header_petrol_station,
-                        parent, false))
-            }
-        }
-        return viewHolder
+        return StationViewHolder(LayoutInflater.from(parent!!.context).inflate(R.layout.cell_petrol_station,
+                parent, false), petrolStationListener)
+
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        when (holder.itemViewType) {
-            STATION_ITEM -> {
-                val stationViewHolder: StationViewHolder = holder as StationViewHolder
-                val station: PetrolStation = this.petrolStationsList[position] as PetrolStation
-                stationViewHolder.setStation(station)
-            }
-            else -> {
-                val headerViewHolder: HeaderViewHolder = holder as HeaderViewHolder
-                val header = this.petrolStationsList[position] as String
-                headerViewHolder.setHeaderTitle(header)
-            }
-        }
-    }
-
-    override fun getItemViewType(position: Int): Int {
-        return if (petrolStationsList[position] is PetrolStation) {
-            STATION_ITEM
-        } else {
-            STATION_HEADER
-        }
+        val stationViewHolder: StationViewHolder = holder as StationViewHolder
+        val station: PetrolStation = this.petrolStationsList[position] as PetrolStation
+        stationViewHolder.setStation(station)
     }
 
     override fun getItemCount(): Int {
         return petrolStationsList.size
     }
 
-    fun updateDataSet(stations: ArrayList<Any>) {
+    fun updateDataSet(stations: ArrayList<PetrolStation>) {
         this.petrolStationsList.clear()
         this.petrolStationsList.addAll(stations)
         this.notifyDataSetChanged()
@@ -107,13 +76,6 @@ class SearchAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
             } else {
                 itemView.petrolStationDistanceTextView.text = itemView.context.getString(R.string.message_unavailable_location)
             }
-        }
-    }
-
-    class HeaderViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        private val headerTitle = itemView.headerTextView
-        fun setHeaderTitle(header: String) {
-            headerTitle.text = header
         }
     }
 }
